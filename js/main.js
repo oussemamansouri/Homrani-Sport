d03b53effdb00a2eca7465092c6159d47255b164
oussemamansouri <99543574+oussemamansouri@users.noreply.github.com>
2023-07-30 22:50:46 +0100

    Add files via upload
    Homrani Sport

---
+++ js/main.js
@@ -0,0 +1,50 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+})(jQuery);
+
+function cal(F){
+
+var w = F.weight.value ;
+var h =F.height.value ;
+    
+
+if(w ==""){ alert("Please enter your weight"); return false;}
+if(h ==""){ alert("Please enter your height"); return false;}
+alert("Your Body Mass Index is :"+ Math.round(w /(h*h)  ).toFixed(2));
+
+
+
+
+
+}
